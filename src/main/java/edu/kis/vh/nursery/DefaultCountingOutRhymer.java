f7b9af611d4a14b0d411b2fc4005c523a8c71dab
Mateusz Oginski <235936@edu.p.lodz.pl>
2022-05-07 13:57:03 +0200

    8. Modyfikacja widocznosci atrybutow klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,11 +1,11 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
-    public static final int SIZE = 12;
-    public static final int START_VAL = -1;
+    private static final int SIZE = 12;
+    private static final int START_VAL = -1;
     private final int[] numbers = new int[SIZE];
 
-    public int total = START_VAL;
+    private int total = START_VAL;
 
     public void countIn(int in) {
         if (!isFull())
